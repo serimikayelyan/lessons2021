fc41cdc4df5d45ea60aec867eda1e8a65b39cea4
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-02 19:30:04 +0400

    tcal

---
+++ calculator.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+function cal
+{
+
+echo "Enter fist number"
+read a
+echo "Enter second number"
+read b
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4.Divition"
+echo "5.Square_root"
+echo "6.Division_balance"
+#echo "7.COS"
+#echo "8.SIN"
+echo "CHOOSE"
+
+read ch
+case $ch in
+     "1" )res=`echo $a + $b | bc`
+        echo "Sum is $res"
+        ;;
+     "2" )res=`echo $a - $b | bc`
+        echo "Sub is $res"
+        ;;
+      "3" )res=`echo $a \* $b | bc`
+        echo "Mul is $res"
+        ;;
+     "4" )res=`echo $a / $b | bc`
+        echo "Div is $res"
+        ;;
+      "5" )res=`echo "sqrt($a * $b)" | bc `
+        echo "Square_root is $res"
+         ;;
+     "6" )res=`echo $a % $b | bc`
+        echo "Balance is $res"
+        ;;
+     #"7" ) res1= `Math.cos($a)`
+           #res2= Math.cos($b)`
+           #echo "Cos(a)= $res1"
+           #echo "Cos(b)= $res2"
+           #;;
+
+esac
+}
+cal
+####???
+read -p "For continue input Y or N :" ans
+while [[ $ans -eq "Y" ]]
+do
+        cal
+        read -p "For continue input Y or N :" ans
+
+
+
+if [[ $ans -eq "N" ]]
+then
+   break
+fi
+done
+
