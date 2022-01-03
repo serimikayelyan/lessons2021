e0bb55fd8cbd92b4ccc800f1d9d5b0b774030207
shushangevorgian <shushan.g003@gmail.com>
2022-01-03 17:19:57 +0400

    calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter Two numbers : "
+read u
+read e
+
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read choice
+
+ if [[ $choice -eq 1 ]] ; then
+	 echo "$u+$e" | bc
+ elif [[ $choice -eq 2 ]] ; then
+	 echo "$u-$e" | bc
+ elif [[ $choice -eq 3 ]] ; then
+	 echo "$u*$e" | bc
+elif [[ $choice -eq 4 ]] ; then
+	echo "$u/$e" | bc
+ fi
