0a6f4f96ec93e0fb3b2d540f613139e0c1e4530c
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-05 22:14:20 +0400

    arrays

---
+++ task221_220/task221.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+arr=(-1 5 78 0 -10 -98 -23 0.2 41 88 -8)
+count=0
+sum=0
+for i in ${#arr[@]}
+ do
+	 if [[ $i > 0 ]]
+	   then
+		count=$(($count+1))
+		sum=$(($sum + $i))
+	fi
+done
+echo "Migin tvabanakan = $(($sum/$count | bc)) "
+
+
