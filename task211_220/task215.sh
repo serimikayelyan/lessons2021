0ce97d1f99b3a25306411e9479d226f135cbd7b2
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-06 13:42:20 +0400

    modtask215

--- task211_220/task215.sh
+++ task211_220/task215.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-arr=(1 5 8 9 6 3 2 7 2 78)
+arr=(1 5 8 9 6 3 2 7 4 78)
 sum=0
 for i in ${!arr[@]}
 do
-  for j in ${#arr[@]}
-   do
-           if [[ $i -eq 0 ]]
-             then
-                     continue
-           fi
-
-            if [[ $i%2 -eq 0 ]]
-              then
-                     sum=$(($sum+$j))
-            fi
-   done
+	if [[ $i -eq 0 ]]
+	 then
+		 continue
+	fi
+	if [[ $i%2 -eq 0 ]]
+	 then
+		 sum=$(($sum+${arr[i]}))
+		 
+        fi
 done
 echo $sum
-
