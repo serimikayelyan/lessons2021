603a75c189440175811df7585cf5ea1874cfdc23
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-06 16:20:20 +0400

    t234

--- task211_220/task234.sh
+++ task211_220/task234.sh
@@ -9,11 +9,19 @@ if [[ ${#arr[@]} -eq 0 ]]
 fi
 for i in ${arr[@]}
 do
+	if ! [[ $i =~ ^[[:digit:]]+$ ]]
+	 then
+		 echo "input numbers"
+		 exit
+	fi
 	if [[ $i%2 -ne 0 ]]
 	  then
 		  sum=$(($sum+$i))
 		  count=$(($count+1))
 		  s=`echo $sum/$count | bc`
+	else
+		echo "the count of odd numbers is 0"
+		exit
         fi
 done
 echo "Mijin tvb=$s"
