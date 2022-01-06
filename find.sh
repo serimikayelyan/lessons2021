617719406d8d701bb99c84ccd40bb7c73d2aba11
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-06 22:13:14 +0400

    this does not work)

---
+++ find.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#this does not work)
+
+
+find_dir () {
+	for name in ${1}/*
+
+	  do
+		  if [ -d ${name} ]
+		    then
+			    find_dir ${name}
+			    
+		  else
+		     echo ${name}
+	         
+		  fi
+	 done
+
+}
+find_dir ${1}
+
