5f691baafa0b86496a9727e87094459f70bd630f
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-06 13:45:06 +0400

    t216mod

--- task211_220/task216.sh
+++ task211_220/task216.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-arr=(1 5 8 9 6 3 2 7 2 78)
+arr=(1 5 8 9 6 3 2 7 4 78)
 p=1
 for i in ${!arr[@]}
 do
-  for j in ${#arr[@]}
-   do
-           if [[ $i -eq 0 ]]
-             then
-                     continue
-           fi
+        if [[ $i -eq 0 ]]
+         then
+                 continue
+        fi
+        if [[ $i%2 -eq 0 ]]
+         then
+                 p=$(($p*${arr[i]}))
 
-            if [[ $i%2 -eq 0 ]]
-              then
-                     p=$(($p*$j))
-            fi
-   done
+        fi
 done
 echo $p
-
