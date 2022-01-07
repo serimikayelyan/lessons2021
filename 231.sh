42f112c1278df4bd41498f466d351c6446fbc139
shushangevorgian <shushan.g003@gmail.com>
2022-01-07 22:56:19 +0400

    231

---
+++ 231.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+list=( 2 5 1 6 4 7 8 9 5 2 3 )
+k=0
+
+for i in ${list[@]}
+do
+    if [[ $i%2 -eq 0 && $i -ne 0 ]]
+    then
+
+	    f=$(($i*$i))
+	    k=$(($k+$f))
+	fi
+done
+echo $k
