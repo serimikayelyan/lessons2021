172051fce85d8db1c1430fa5346b3625a1ef4a8f
Jora Nersisyan <jnersisyan@internet.ru>
2022-01-08 12:14:55 +0400

    modified find

--- find.sh
+++ find.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-
-if [[ -n "$1" ]]; then
-	if [[ -n "$2" ]]; then
-		ls -Rp $1 | grep -v / | grep "$2"
+for i in $1/
+do
+	if [[ -n "$i" ]]; then
+		if [[ -n "$2" ]]; then
+			result=`ls -Rp $i | grep -v / | grep "$2"`
+			if [[ -n $result ]]; then
+				echo "`realpath $result`"
+			else
+				echo "File does not exist"
+			fi
+		else
+			echo "Enter the file name"
+		fi
 	else
-		echo "Enter the file name"
+		echo "Enter the path name"
 	fi
-else
-	echo "Enter the path"
-fi
+done
+
+
