3ae32c39566a5d8c38f082c2753d0b1dc086bc16
Ruben <ruben.agoyan@gmail.com>
2022-01-09 12:59:00 +0400

    substr changes

--- homework/bash_scripting/task_group_5/get_sub.sh
+++ homework/bash_scripting/task_group_5/get_sub.sh
@@ -3,7 +3,32 @@
 function main
 {
     substrings=($@)
+    
+    if [[ $# -eq 0 ]]; then
+	echo No substring is given "(input substrings as argument)"
+	return 2
+    fi
+
+    echo DISCLAIMER: Search is case-sensetive
+
     read -p "Enter string: " string
 
+    if [[ ${#string} -eq 0 ]]; then
+	echo No input
+	return 1
+    fi
 
+    for i in ${substrings[@]}; do
+	for ((j=0;j<=$((${#string} - ${#i}));j++)); do
+	    if [[ ${string:$j:${#i}} == $i ]]; then
+		echo $i: True
+		break
+	    elif [[ $j -eq $((${#string} - ${#i})) ]]; then
+		echo $i: False
+		break
+	    fi
+	done
+
+    done
 }
+main $@
