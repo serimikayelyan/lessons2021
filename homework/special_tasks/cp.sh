66f8285751ac1c61aeb239b923ed803a76e4b2e8
JIN4HOCTb <sarnarek2003@mail.ru>
2022-01-12 17:07:25 +0400

    cp.sh

--- homework/special_tasks/cp.sh
+++ homework/special_tasks/cp.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+function my_cp
+{
+    file=$1
+    dest=$2
+    dest_file_name=${file##*/}
+    dest_path=$dest
+    if [[ ! -f $file ]]; then
+        echo "give me file "
+        return 1
+    fi
+    
+    if [[ ! -d $dest ]]; then
+        dest_path=${dest##/*}
+        if [[ ! -d ${dest_path} ]]; then
+            echo "give me existing directory"
+        else
+            dest_file_name=${dest##*/}
+        fi
+    fi
+    
+    cat $file > $dest_path/$dest_file_name
+    
+}
 
-file1=$1
-dir=$2
-
-if [[ -d $dir ]]; then
-	
-else
-	echo "no find directory"
-fi
-
-if [[ -e $file ]]; then
-	
-else
-	echo "no find file"
-fi
-
-if 
-fi
+my_cp $1 $2
