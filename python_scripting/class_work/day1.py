587ec39aa0e032c74d3c82cb03bb51060f60cb14
Ruben <ruben.agoyan@gmail.com>
2022-01-16 15:51:13 +0400

    tweaks

---
+++ python_scripting/class_work/day1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+#----------------------------------------
+# square perimeter and area
+
+# side=int(input("Side lenght: "))
+# print(f"P: {4 * side}, S: {side ** 2}")
+
+#----------------------------------------
+# get 3 numbers, and calculate max
+
+# max_val = int(input("number a: "))
+# b = int(input("number b: "))
+# c = int(input("number c: "))
+# 
+# if max_val < b:
+#     max_val = b
+# if max_val < c:
+#     max_val = c
+# 
+# print("maximum value:", max_val)
+
+#----------------------------------------
+# get 3 numbers, and sort low->great
+
+# a = int(input("number a: "))
+# b = int(input("number b: "))
+# c = int(input("number c: "))
+# 
+# if a > b:
+#     a, b = b, a
+#     # temp = a; a = b; b = temp
+# if b > c:
+#     c, b = b, c
+# if a > b:
+#     a, b = b, a
+# 
+# print(a, b, c)
+
+#----------------------------------------
+# FizzBuzz
+
+#number = int(input("number: "))
+#
+#if number % 15 == 0:
+#    print("FizzBuzz")
+#elif number % 3 == 0:
+#    print("Fizz")
+#elif number % 5 == 0:
+#    print("Buzz")
+
+#----------------------------------------
