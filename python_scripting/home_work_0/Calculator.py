0931e1e8eff07ccbfb507d6ceeae9ab56145db76
Ruben <ruben.agoyan@gmail.com>
2022-01-18 16:19:20 +0400

    Looped

---
+++ python_scripting/home_work_0/Calculator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3.8
+
+while True:
+    a = input("input a number: ")
+    if a.lower() == "c":
+        break
+    if not ((a.count(".") == 1 and a.replace(".","").isdigit()) or a.isdecimal()):
+        print("NAN")
+        exit()
+
+    oper = input("input operation: ")
+    if oper.lower() == "c":
+        break
+    if len(oper) != 1 or not oper in "+-*/":
+        print("Unsupported operation")
+        exit()
+    
+    b = input("input a number: ")
+    if b.lower() == "c":
+        break
+    if not ((b.count(".") == 1 and b.replace(".","").isdigit()) or b.isdecimal()):
+        print("NAN")
+        exit()
+    
+    a = float(a)
+    b = float(b)
+    
+    if oper == "+":
+        print(a + b)
+    if oper == "-":
+        print(a - b)
+    if oper == "*":
+        print(a * b)
+    if oper == "-":
+        print(a / b)
+
+#eval(input("expr"))
