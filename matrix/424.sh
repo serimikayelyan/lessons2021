4573591f7aae8bb3e4e200e7cf9fd1ed740675c1
shushangevorgian <shushan.g003@gmail.com>
2022-01-18 19:47:34 +0400

    matrix

---
+++ matrix/424.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -A matrix
+m=$1
+sum=0
+num=0
+var=0
+for ((i=0;i<m;i++)); do
+        for ((j=0;j<m; j++)); do
+                matrix[$i,$j]=$(($i+$j))
+                let var=${matrix[$i,$j]}
+                if [[ $var%2 -ne 0 && $j<$i ]]; then
+                        sum=$(($sum+$var))
+                        num=$(($num+1))
+                fi
+                echo -n "${matrix[$i,$j]} "
+        done
+        echo
+done
+ave=$(($sum/$num))
+echo $ave
+
