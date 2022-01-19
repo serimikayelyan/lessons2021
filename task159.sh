821f34bfb63647c52d09579b8d309caa0932ce46
MM109876543210 <mariam.mardoyan1@gmail.com>
2022-01-19 15:26:06 +0400

    del

--- task159.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source functions.sh
-function main 
-{
-        task159 $1
-}
-main $1
--- task163.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source functions.sh
-function main 
-{
-        task163 $1
-}
-main $1
