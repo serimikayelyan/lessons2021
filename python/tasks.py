a89c2f5695bafc4772e5c0253d933b99f8f900a3
Vahe Mkrtchyan <vahemkrtchyan1995@gmail.com>
2022-01-19 15:30:30 +0400

    homework

---
+++ python/tasks.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+#   Task__1
+
+#sentence = input("Enter a sentence: ")
+#num = 0
+#for i in sentence:
+#    if i.isalpha():
+#       num += 1
+#print(num)
+
+
+#   Task__2
+
+#msen = input("Enter a sentence: ")
+#rsen = ""
+#for i in msen:
+#        if i in "aeiou":
+#            rsen += i
+#print(rsen)
+
+
+#    Task__3
+
+#txt = input("Enter a sentence: ")
+#new = txt.swapcase()
+#print(new)
+
+#    Task__4
+
+#sentence = input("Enter a sentence: ")
+#num = 0
+#for i in sentence:
+#    if i.isnumeric():
+#        num += 1
+#print(num)
+
+
+#    Task__5
+
+#sentence = input("Enter a sentence: ")
+#num = 0
+#for i in sentence:
+#    if not i.isalpha():
+#        num += 1
+#print(num)
+
+
+#    Task__6
+
+#sentence = input("Enter a sentence: ")
+#mstr = ""
+#for i in range(len(sentence)):
+#        mstr = sentence[i] + mstr
+#print(mstr)
+
+
+#    Task__9
+
+#print("You need to enter 10 numbers: ")
+#count = 0
+#for i in range(10):
+#    num = int(input('Enter a number: '))
+#    if num % 2 == 0:
+#        count += 1
+#print('Ther are', count, 'twain numbers')
+
+
+
+
+
+
