e6de53dd9838256118f12a95671d6e88ca65ea8c
tatevik avetisyan <avetisyant125@gmail.com>
2022-01-19 22:41:53 +0400

    calc_2

--- homework/python/calc.py
+++ homework/python/calc.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3.8
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+while True:
 
-select = input("Select operators: ")
+	num1 = input("Enter first number: ")
 
+	if num1.lower() == "c":
+		break
+
+	num2 = input("Enter second number: ")
+
+	if num2.lower() == "c":
+		break
+
+	select = input("Select operators: ")
+
+	if select.lower() == "c":
+		break
+
+	num1 = int(num1)
+	num2 = int(num2)
 #if not num1.isdigit():
 #	print("Error")
 #	sys.exit()
 #
-if select == "+":
-    print(num1, "+", num2, "=", num1+num2)
-
-elif select == "-":
-    print(num1, "-", num2, "=", num1-num2)
-
-elif select == "*":
-    print(num1, "*", num2, "=", num1*num2)
-
-elif select == "/":
-    print(num1, "/", num2, "=", num1/num2)
-
-
+	
+	if select == "+":
+	    print(num1, "+", num2, "=", num1+num2)
+	
+	elif select == "-":
+	    print(num1, "-", num2, "=", num1-num2)
+	
+	elif select == "*":
+	    print(num1, "*", num2, "=", num1*num2)
+	
+	elif select == "/":
+	    print(num1, "/", num2, "=", num1/num2)
+	
+	
