f23f99f8e9bced6cf006811553645d8c96dd0bb8
Armen Melkonyan <armen.melkonyan90@gmail.com>
2022-01-21 00:28:34 +0400

    tasks 1 to 4

--- Python_homeworks/Homework_1/task_12.py
+++ Python_homeworks/Homework_1/task_12.py
@@ -12,17 +12,17 @@ else:
          if sen.count(i) >= maxvalue:
             maxvalue = sen.count(i)
             mul = i
-    for i in sen:
-        if sen.count(i) <= minvalue:
+   
+         if sen.count(i) <= minvalue:
             minvalue = sen.count(i)
             lul = i
     for i in sen:
         if i == mul:
             new_sen += lul 
-            continue
+         
         elif i == lul:
              new_sen += mul
-             continue
+         
         else:
              new_sen += i
-    print(new_sen)                          
+    print(new_sen)                             
