bf4dba7d799fd8352d2ffc0d42cc5b29668e7af4
SahakanushTerteryan <sahakanush.terteryan2003@gmail.com>
2022-01-21 00:44:30 +0400

    7

--- Python_/task_sentences/t7.py
+++ Python_/task_sentences/t7.py
@@ -3,16 +3,12 @@
 text = input("Text: ")
 
 s = text.split()
-max_ = len(s[0])
-index=0
+max_ = 0
+index = 0
 
 while index < len(s):
     if len(s[index]) > max_ :
         max_=len(s[index])
         a = s[index]
     index+=1
-
-if max_ == len(s[0]):
-    print(s[0])
-else:
-    print(a)
+print(a)
