33f98722e0c7cad4c516f6950b2c4f1525e8864a
Hakob <hakob.mkoyan771@gmail.com>
2022-01-21 02:11:34 +0400

    bugs fixed

--- Python/Tasks1/Special/Calculator.py
+++ Python/Tasks1/Special/Calculator.py
@@ -13,55 +13,65 @@ while True:
 
     valid = 0
     for i in expression:
-        if i == "+":
+        if i == "+" or i == "-" or i == "*" or i == "/":
             valid += 1
-        if i == "-":
-            valid += 1
-        if i == "*":
-            valid += 1
-        if i == "/":
-            valid += 1
-    if valid > 1:
+
+    if valid > 1 or valid == 0:
         print("Read instruction")
     else:
         if expression == "exit()":
             break
         for i in range(len(expression)):
+            
+            argValid = 0
+
             if expression[i] == "+":
                 num1 = expression[:i]
                 num2 = expression[i+1:]
-                if num1 == "" or num2 == "":
+                if len(num1) == 0 or len(num2) == 0:
                     print("Missing Argument")
+                    argValid = 1
                 elif num1.isdigit() == False or num2.isdigit() == False:
                     print("Expression should be numbers!")
+                    argValid = 1
                 else:
                     sum = int(num1) + int(num2)
+
             if expression[i] == "-":
                 num1 = expression[:i]
                 num2 = expression[i+1:]
-                if num1 == "" or num2 == "":
+                if len(num1) == 0 or len(num2) == 0:
                     print("Missing Argument")
+                    argValid = 1
                 elif num1.isdigit() == False or num2.isdigit() == False:
                     print("Expression should be numbers!")
+                    argValid = 1
                 else:
                     sum = int(num1) - int(num2)
+
             if expression[i] == "*":
                 num1 = expression[:i]
                 num2 = expression[i+1:]
-                if num1 == "" or num2 == "":
+                if len(num1) == 0 or len(num2) == 0:
                     print("Missing Argument")
+                    argValid = 1
                 elif num1.isdigit() == False or num2.isdigit() == False:
                     print("Expression should be numbers!")
+                    argValid = 1
                 else:
                     sum = int(num1) * int(num2)
+
             if expression[i] == "/":
                 num1 = expression[:i]
                 num2 = expression[i+1:]
-                if num1 == "" or num2 == "":
+                if len(num1) == 0 or len(num2) == 0:
                     print("Missing Argument")
+                    argValid = 1
                 elif num1.isdigit() == False or num2.isdigit() == False:
                     print("Expression should be numbers!")
+                    argValid = 1
                 else:
                     sum = int(num1) // int(num2)
-        print(sum)               
+        if argValid == 0:            
+            print(sum)
 
