4c67c379f229d0a63e9f25e804e7b7df5562f734
SahakanushTerteryan <sahakanush.terteryan2003@gmail.com>
2022-01-21 00:54:53 +0400

    8

--- Python_/task_sentences/t8.py
+++ Python_/task_sentences/t8.py
@@ -4,15 +4,11 @@ text = input("Text: ")
 max_ = 0
 countt = 0
 index = 0
-a = 0  # a popoxakany chhaytararelu depqum nuynpes ashxatum e, chgitem inchu
+a = 0  
 while index < len(text):
     countt = text.count(text[index])
     if countt > max_ :
         max_ = countt
         a = text[index]
     index+=1
-if max_ == 0:
-    print(text[0])
-else:
-    print(a, "used-",max_)
-
+print(a, "used_", max_)
