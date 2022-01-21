9c2e0d3f5048191361fb09993ae83df4e68c8f78
Heghine <nalbandyan009@mail.ru>
2022-01-21 22:32:27 +0400

    1

--- homework/Python/for_22.01.2022/1.py
+++ homework/Python/for_22.01.2022/1.py
@@ -1,12 +1,10 @@
 #!/usr/bin/Python3
 
-
-
 for i in range(100,1000,2):    
    
    a = i//100
    b = (i-100*a)//10
    c = i-100*a-10*b
-   if i % 2 == 0 and b == 0 and c != 4:
+   if b == 0 and c != 4:
     print (i)
   
