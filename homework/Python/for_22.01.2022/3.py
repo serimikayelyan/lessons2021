9e497c6a3f2c114a8eaa87b28318adcc9f49edc7
Heghine <nalbandyan009@mail.ru>
2022-01-22 00:10:17 +0400

    3

---
+++ homework/Python/for_22.01.2022/3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+while True:
+    sentence = input(" Input sentence : ")
+    number = int(input("Input number : ")) 
+    sentence = sentence[ :-number]
+    print(sentence)
+    
+
+
+
